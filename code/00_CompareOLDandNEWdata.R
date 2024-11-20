883dc418d33f96d31dae4161018e33bc3be838bb
Jeffrey <Jparkerpowell@gmail.com>
2024-11-20 09:26:46 +0100

    updated R code slightly

--- code/00_CompareOLDandNEWdata.R
+++ code/00_CompareOLDandNEWdata.R
@@ -2,7 +2,7 @@ library("dplyr")
 library("diffdf")
 
 data1 <- read.csv("data/cbs_basic_macro_SEASONCORRECTED_qt_2024_11_18.csv", sep = ",")
-data2 <- read.csv("data/cbs_basic_macro_SEASONCORRECTED_qt_2024_11_16.csv", sep = ",")
+data2 <- read.csv("data/cbs_basic_macro_SEASONCORRECTED_qt_2024_11_20.csv", sep = ",")
 
 all_equal(data1, data2)  
 
